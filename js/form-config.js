cb7995f7a944b24c9b19be9e94066832459bc52e
DVEB-BLG <DVEB-BLG@users.noreply.github.com>
2026-08-07 07:28:51 +0000

    URL бэкенда (исправленный)

--- js/form-config.js
+++ js/form-config.js
@@ -3,4 +3,4 @@
  * 
  * URL веб-приложения Google Apps Script.
  */
-window.DVEB_FORM_BACKEND = 'https://script.google.com/macros/s/AKfycbw1XTgFeA16I3TXyzMzojCeIAIesQuLceG58hkrEkFQ1uQlPalslibDQtaL0bTyM16B/exec';
+window.DVEB_FORM_BACKEND = 'https://script.google.com/macros/s/AKfycbw7XszfZ0x8OuiR7z-EVtgCc-jyJw6uZEUIMsUqnhtpjfUhW5m_IicP1C1a7rwStXkj/exec';
